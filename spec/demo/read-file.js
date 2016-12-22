c10bb405daf30040788a8f972f3830a06faca52e
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2016-12-22 03:42:49 +0300

    commit

--- spec/demo/read-file.js
+++ spec/demo/read-file.js
@@ -1,5 +1,10 @@
 var fs = require('fs');
-var content = fs.readFileSync('file.txt');
 
-console.log(decodeURIComponent(content));
+// var content = fs.readFileSync('file.txt');
+// console.log(decodeURIComponent(content));
+
+fs.readFile('file.txt', function (err, content){
+  console.log(decodeURIComponent(content));
+});
+
 console.log('The end');
