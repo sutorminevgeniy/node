31a10f7c09fabcfdecae11c60466e29eff6da74c
Sutormin Evgeniy <Sutormin@competentum.ru>
2016-12-28 18:44:57 +0300

    commit

--- spec/demo/5/app.js
+++ spec/demo/5/app.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 app.listen(8080);
 
+var names = {
+  'john': 'admin',
+  'mike': 'manager',
+  'ivan': 'user'
+}
+
 app.get('/', function(request, response){
   response.sendfile(__dirname + '/test.html');
+});
+app.get('/user/:name', function(request, response){
+  var role = names[request.params.name]
 });
